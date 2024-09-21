ea899b18b559f5a7459f2097b58a0ffb45e9ff68
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2024-09-21 22:22:42 +0300

    upload exer 3 code

--- mathematical-modelling/task-1/3.py
+++ mathematical-modelling/task-1/3.py
@@ -9,7 +9,8 @@ def y(x):
 plt.xlabel("x")
 plt.ylabel("y")
 xs = np.linspace(-1, 4, 1000)
-plt.plot(xs, y(xs), color='red', label=f'$y(x)=\\frac{{5}}{{4}}e^{{-2x}}+\\frac{{x}}{{2}}-\\frac{{1}}{{4}}$')
+label = '$y(x)=\\frac{5}{4}e^{-2x}+\\frac{x}{2}-\\frac{1}{4}$'
+plt.plot(xs, y(xs), color='red', label=label)
 plt.scatter([0], [1], color='blue', label=f'$y(0)=1$')
 plt.legend()
 # %%
