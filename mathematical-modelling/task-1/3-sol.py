d1756953d9ce60f65366689ccd62779781c722e5
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2024-09-21 14:15:17 +0300

    finish modelling task 1

---
+++ mathematical-modelling/task-1/3-sol.py
@@ -0,0 +1,10 @@
+# %%
+from sympy import *
+x = symbols('x')
+y = Function('y')(x)
+equation = Eq(y.diff(x) + 2 * y,x)
+general_solution = dsolve(equation)
+initial_condition = {y.subs(x, 0): 1}
+constants = solve(general_solution.rhs.subs(x, 0) - 1)
+general_solution.subs('C1', constants[0])
+# %%
